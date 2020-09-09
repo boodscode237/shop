4f5d3db8c4e7dc5e1831f02b27bb7c5a51bb5d50
Abel237 <abodobrice@gmail.com>
2020-09-10 01:04:30 +0500

    firebase authentification was added, database created

--- src/App.js
+++ src/App.js
@@ -5,21 +5,54 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SingnInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth, creatUserProfileDocument } from './firebase/firebase.utils';
 
 
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SingnInAndSignUpPage} />
-      </Switch>
-      {/* <Homepage /> */}
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await creatUserProfileDocument(userAuth)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      else {
+        this.setState({ currentUser: userAuth })
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SingnInAndSignUpPage} />
+        </Switch>
+        {/* <Homepage /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
